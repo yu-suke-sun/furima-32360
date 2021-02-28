68cac423cce908c58774a99c76b09c6f001eeb00
yu-suke-sun <76049703+yu-suke-sun@users.noreply.github.com>
2021-02-28 12:38:55 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,21 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true
   validates :birth_date, presence: true
 
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :family_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カナ文字を使用してください' } do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
+
+  with_options presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '英字と数字の両方を含めて設定してください' } do
+    validates :password
+  end
+
+
   has_many :items
   has_many :purchases
 end
