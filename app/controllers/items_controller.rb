7df7d05bb23af2ac140a88ff4efc004d16cab464
yu-suke-sun <76049703+yu-suke-sun@users.noreply.github.com>
2021-03-15 12:09:21 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,9 +23,17 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else 
+      render :edit
+    end
   end
 
 
