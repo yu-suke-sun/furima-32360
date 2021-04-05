bc1a273c824081c7065bd00f531d8bd746baac73
yu-suke-sun <76049703+yu-suke-sun@users.noreply.github.com>
2021-04-05 18:00:58 +0900

    プライベートメソッドの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit, :update, :destroy]
   before_action :item_find, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -47,8 +47,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    item = Item.find(params[:id])
-    redirect_to action: :index unless current_user.id == item.user_id
+    redirect_to action: :index unless current_user.id == @item.user_id
   end
 
   def item_find
