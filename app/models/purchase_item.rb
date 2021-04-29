b4ad82aa2cf5fa249e2a6cde194618470e7c6e81
yu-suke-sun <76049703+yu-suke-sun@users.noreply.github.com>
2021-04-29 22:43:31 +0900

    バリデーションの修正

--- app/models/purchase_item.rb
+++ app/models/purchase_item.rb
@@ -10,7 +10,7 @@ class PurchaseItem
     validates :area_id, numericality: { other_than: 1, message: 'Select' }
     validates :municipality
     validates :address
-    validates :telephone_number, format: { with: /\A[0-9]{11}\z/, message: 'Input only number' }
+    validates :telephone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'Input only number' }
   end
 
   def save
